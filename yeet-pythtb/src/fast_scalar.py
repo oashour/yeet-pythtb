1d775dc2d702a1bd6bf72a143ec39f42089a25b9
Omar Ashour <omar.ashour22@gmail.com>
2024-03-26 01:06:52 -0700

    Fully rewrite scalar solver

--- yeet-pythtb/src/fast_scalar.py
+++ yeet-pythtb/src/fast_scalar.py
@@ -1,108 +1,58 @@
-from numba import njit, prange
-from numba.np.ufunc import parallel
-from numba.types import float64,int64,int32,complex128
 import numpy as np
+from numba import njit, prange
+from numba.types import float64, complex128
 
 
-@njit
-def _nicefy_eval(eval):
-    "Sort eigenvaules and eigenvectors, if given, and convert to real numbers"
-    # first take only real parts of the eigenvalues
-    eval=eval.real
-    # sort energies
-    args=eval.argsort()
-    eval=eval[args]
-    return eval
-@njit
-def _nicefy_eig(eval,eig):
-    "Sort eigenvaules and eigenvectors, if given, and convert to real numbers"
-    # first take only real parts of the eigenvalues
-    eval=eval.real
-    # sort energies
-    args=eval.argsort()
-    eval=eval[args]
-    eig=eig[args]
-    return (eval,eig)
+@njit(parallel=True, fastmath=True)
+def solve_all(
+    dim_k, per, orb, norb, nsta, site_energies, hst, hind, hR, k_list, eig_vectors=False
+):
+    nk = len(k_list)  # number of k points and hoppings
+    # Initialize arrays eigvecs/eigvals
+    eigvals = np.zeros((nsta, nk), dtype=float64)
+    eigvecs = np.zeros((nsta, nk, norb), dtype=complex128)
 
-#scalar implementation
-# @njit('complex128[:,::1](int64,int32[::1],float64[:,::1],int64,float64[::1],complex128[::1],int32[:,::1],int32[:,::1],float64[::1])')
-# Parallel = true here leads to bizzare noisy bands
-@njit(parallel=False,fastmath=True)
-def gen_ham(dim_k,per,orb,norb,site_energies,hst,hind,hR,k_input):
-        """Generate Hamiltonian for a certain k-point,
-        K-point is given in reduced coordinates!"""
-        kpnt = k_input
-        norb = orb.shape[0]
-        ham=np.zeros((norb,norb),dtype=complex128)
-        # modify diagonal elements
-        for i in range(norb):
-            ham[i,i]=site_energies[i]
-        # go over all hoppings
-        for h in range(hst.shape[0]):
-            # get all data for the hopping parameter
-            amp=hst[h]
-            i = hind[h,0]
-            j = hind[h,1]
-            # in 0-dim case there is no phase factor
-            if dim_k>0:
-                ind_R = hR[h]
-                # vector from one site to another
-                rv = -orb[i,:] + orb[j,:] + ind_R
-                # Take only components of vector which are periodic
-                rv = rv[per]
-                # Calculate the hopping, see details in info/tb/tb.pdf
-                phase=np.exp((2.0j) * np.pi * np.dot(kpnt,rv))
-                amp = amp * phase
-            # add this hopping into a matrix and also its conjugate
+    # Generate the Hamiltonian for all k points
+    H_k = generate_H(dim_k, per, orb, norb, site_energies, hst, hind, hR, k_list)
 
-            ham[i,j] += amp
-            ham[j,i] += np.conjugate(amp)
-        return ham
-#scalar implementation
-# @njit('Tuple((float64[::1],complex128[:,::1]))(complex128[:,::1],boolean)')
-@njit(parallel=True,fastmath=True)
-def sol_ham(ham,eig_vectors=False):
-        """Solves Hamiltonian and returns eigenvectors, eigenvalues"""
-        # reshape matrix first
-        ham
-        # check that matrix is hermitian
-        #if np.real(np.max(ham-ham.T.conj()))>1.0E-9:
-        #    raise Exception("\n\nHamiltonian matrix is not hermitian?!")
-        #solve matrix
-        eig = np.zeros(ham.shape,dtype="complex128")
-        if not eig_vectors: # only find eigenvalues
-            eval=np.linalg.eigvalsh(ham)
-            # sort eigenvalues and convert to real numbers
-            eval=_nicefy_eval(eval)
-        else: # find eigenvalues and eigenvectors
-            (eval,eig)=np.linalg.eigh(ham)
-            # transpose matrix eig since otherwise it is confusing
-            # now eig[i,:] is eigenvector for eval[i]-th eigenvalue
-            eig=eig.T
-            # sort evectors, eigenvalues and convert to real numbers
-            (eval,eig)=_nicefy_eig(eval,eig)
-            # reshape eigenvectors if doing a spinfull calculation
-        return (eval,eig)
-#scalar implementation
-# @njit('Tuple((float64[:,::1],complex128[:,:,::1]))(int64,int32[::1],float64[:,::1],int64,int64,float64[::1],complex128[::1],int32[:,::1],int32[:,::1],float64[:,::1],boolean)',parallel=True)
-@njit(parallel=True,fastmath=True)  
-def solve_all(dim_k,per,orb,norb,nsta,site_energies,hst,hind,hR,k_list,eig_vectors=False):
-    nkp=len(k_list) # number of k points
-    # first initialize matrices for all return data
-    #    indices are [band,kpoint]
-    ret_eval = np.zeros((nsta,nkp),dtype="float64")
-    #    indices are [band,kpoint,orbital,spin]
-    ret_evec = np.zeros((nsta,nkp,norb),dtype="complex128")
-    # go over all kpoints
-    for i in prange(k_list.shape[0]):
-        # generate Hamiltonian at that point
-        ham = gen_ham(dim_k,per,orb,norb,site_energies,hst,hind,hR,k_list[i])
-        # solve Hamiltonian
-        ret_eval[:,i], ret_evec[:,i,:] = sol_ham(ham,eig_vectors=eig_vectors)
-    # return stuff
-    #if not eig_vectors:
-    #    # indices of eval are [band,kpoint]
-    #    return ret_eval, np.zeros((1,1,1),dtype="complex128")
-    #else:
-    # indices of eval are [band,kpoint] for evec are [band,kpoint,orbital,(spin)]
-    return (ret_eval,ret_evec)
+    # Solve the Hamiltonian for all k points
+    if eig_vectors:
+        for k in prange(len(k_list)):
+            eigvals[:, k], eigvecs[:, k, :] = np.linalg.eigh(H_k[k])
+    else:
+        for k in prange(len(k_list)):
+            eigvals[:, k] = np.linalg.eigvalsh(H_k[k])
+
+    return (eigvals, eigvecs)
+
+
+@njit(parallel=True, fastmath=True)
+def generate_H(dim_k, per, orb, norb, site_energies, hst, hind, hR, k_list):
+    nk, nhop = len(k_list), len(hst)  # number of k points and hoppings
+    H_k = np.zeros((nk, norb, norb), dtype=complex128)
+    # @njit(parallel=True) friendly implementation of
+    # H_k = np.repeat(np.diag(site_energies+0j)[None, :, :], nkp, axis=0)
+    for k in prange(nk):
+        np.fill_diagonal(H_k[k], site_energies)
+    all_i, all_j = hind.T
+    amp = np.zeros((nk, nhop), dtype=np.complex128)
+    if dim_k > 0:
+        # r_{ij} = \tau_j - \tau_i + R
+        r_ij = -orb[all_i, :] + orb[all_j, :] + hR
+        r_ij = r_ij[:, per]  # Neglect non-periodic components
+        # In parallel, lots of stuff doesn't work...
+        # Can we find a way to do this without a loop that works in parallel?
+        phase = np.exp(2 * np.pi * 1j * (k_list @ r_ij.T))
+        for k in prange(nk):
+            amp[k, :] = hst * phase[k, :]
+    else:
+        # In 0-dim case there is no phase factor
+        for k in prange(nk):
+            amp[k, :] = hst
+
+    # Numba doesn't support np.add.at, need loop
+    for k in prange(nk):
+        for h, (i, j) in enumerate(zip(all_i, all_j)):
+            H_k[k, i, j] += amp[k, h]
+            H_k[k, j, i] += np.conjugate(amp[k, h])
+    return H_k
